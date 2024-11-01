a7aab3d4d06f1661fefa906c487c29f587835346
anhdnhe163745 <anhdnhe163745@fpt.edu.vn>
2024-11-01 10:31:06 +0700

    add createTreatment

---
+++ HospitalManager/HospitalLibrary/Repository/ITreatmentPlanRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using HospitalLibrary.DataAccess;
+
+namespace HospitalLibrary.Repository
+{
+    public interface ITreatmentPlanRepository
+    {
+        TreatmentPlan AddTreatmentPlan(TreatmentPlan treatmentPlan);
+        IEnumerable<TreatmentPlan> GetAllTreatmentPlans();
+        TreatmentPlan GetTreatmentPlantById(int treatmentPlanID);
+        bool UpdateTreatmentPlan(TreatmentPlan treatmentPlan);
+        bool DeleteTreatmentPlan(int treatmentPlanId);
+        
+    }
+}
---
+++ HospitalManager/HospitalLibrary/Service/TreatmentPlanService.cs
@@ -0,0 +1,47 @@
+﻿using HospitalLibrary.DataAccess;
+using HospitalLibrary.Repository;
+using System;
+using System.Collections.Generic;
+
+namespace HospitalLibrary.Service
+{
+    public class TreatmentPlanService : ITreatmentPlanService
+    {
+        private readonly ITreatmentPlanRepository _treatmentPlanRepository;
+
+        public TreatmentPlanService(ITreatmentPlanRepository treatmentPlanRepository)
+        {
+            _treatmentPlanRepository = treatmentPlanRepository;
+        }
+
+        public bool AddTreatmentPlan(TreatmentPlan treatmentPlan)
+        {
+            if (treatmentPlan == null) throw new ArgumentNullException(nameof(treatmentPlan));
+            treatmentPlan.CreatedAt = DateTime.Now;
+            _treatmentPlanRepository.AddTreatmentPlan(treatmentPlan);
+            return true;
+        }
+
+        public bool DeleteTreatmentPlan(int id)
+        {
+            return _treatmentPlanRepository.DeleteTreatmentPlan(id);
+        }
+
+        public IEnumerable<TreatmentPlan> GetAllTreatmentPlans()
+        {
+            return _treatmentPlanRepository.GetAllTreatmentPlans();
+        }
+
+        public TreatmentPlan GetTreatmentPlantById(int id)
+        {
+            return _treatmentPlanRepository.GetTreatmentPlantById(id);
+        }
+
+        public bool UpdateTreatmentPlan(TreatmentPlan treatmentPlan)
+        {
+            if (treatmentPlan == null) throw new ArgumentNullException(nameof(treatmentPlan));
+            treatmentPlan.UpdateAt = DateTime.Now;
+            return _treatmentPlanRepository.UpdateTreatmentPlan(treatmentPlan);
+        }
+    }
+}
